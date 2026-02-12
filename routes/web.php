917710e11b7084ab30671656310121d7d8af5b7b
Grigoriy <grisha_koryakov@mail.ru>
2026-02-12 12:46:59 +0500

    lb1: info routes with DTO

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\InfoController;
+
+Route::prefix('info')->group(function () {
+    Route::get('/', [InfoController::class, 'index']);
+    Route::get('/client', [InfoController::class, 'client']);
+    Route::get('/server', [InfoController::class, 'server']);
+    Route::get('/database', [InfoController::class, 'database']);
+});
